bd94b20d82bf11240ad3584c515c790d255da919
Hua-rookie <messihua@126.com>
2025-01-12 16:18:50 +0800

    v0.0

--- commands/run_finetune.sh
+++ commands/run_finetune.sh
@@ -2,7 +2,7 @@
 #### RoPE-512 + YARN-1024 ####
 torchrun --nproc_per_node=4 scripts/train.py \
          configs/c4/length-1024/ce-extra/x+yarn/OLMo-60M-ce-yarn.yaml \
-         --load_path=workspace/OLMo-60M-ce-512-fourier-eye_xavier_norm_0_4-sep_basis_head-ignore_clamp_zero-c4/latest-unsharded
+         --load_path=workspace/OLMo-60M-ce-512-c4/latest-unsharded
 #### FoPE-512 + YARN-1024 ####
 torchrun --nproc_per_node=4 scripts/train.py \
          configs/c4/length-1024/ce-extra/x+yarn/OLMo-60M-ce-fourier-eye_xavier_norm_0_4-sep_basis_head-ignore_clamp_zero-yarn_after.yaml \
@@ -12,7 +12,7 @@ torchrun --nproc_per_node=4 scripts/train.py \
 #### RoPE-512 + FoPE-1024 ####
 torchrun --nproc_per_node=4 scripts/train.py \
          configs/c4/length-1024/ce-extra/x+fope/OLMo-60M-ce-fourier-eye_xavier_norm_0_4-sep_basis_head-ignore_clamp_zero-extra.yaml \
-         --load_path=workspace/OLMo-60M-ce-512-fourier-eye_xavier_norm_0_4-sep_basis_head-ignore_clamp_zero-c4/latest-unsharded
+         --load_path=workspace/OLMo-60M-ce-512-c4/latest-unsharded
 
 ##### FoPE-512 + FoPE-1024 #####
 torchrun --nproc_per_node=4 scripts/train.py \
